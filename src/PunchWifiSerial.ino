d7bfdf9892f4a9bea966e2ec30a160c52efd1413
Thanh Truong <tonhtron@hotmail.com>
2021-10-19 12:15:10 +0700

    Wifi Working-before add wifi connectivity events

--- src/PunchWifiSerial.ino
+++ src/PunchWifiSerial.ino
@@ -1,188 +1,319 @@
-// ESP32 WiFi <-> 3x UART Bridge
-// by AlphaLima
-// modified by Thanh Truong 9/26/2021
-//  * remove BlueTooth
-//  * major rework (2Server<-->2UART at 3client/Server(uart))
-
-
-
 #include "config.h"
 #include <esp_wifi.h>
 #include <WiFi.h>
-
+#include <WiFiClient.h>
+#include <Preferences.h>
 
 #ifdef OTA_HANDLER
 #include <ArduinoOTA.h>
-
 #endif // OTA_HANDLER
+
+//-----------------------------------------------------
+#define wifi_max_try 10             // Number of try
+#define duration_deep_sleep 10      // Sleep until next try
+
+Preferences preferences;//save none-volatile data
+
 //*** HardwareSerial Serial; Default auto create to/from wifi
-HardwareSerial Serial_1(1); //COM1 - debug output
-HardwareSerial* pCOM[NUM_COM] = {&Serial, &Serial_1};
+HardwareSerial serialMcu(1);
+WiFiClient client;//me
 
-#include <WiFiClient.h>
-WiFiServer server_0(SERIAL0_TCP_PORT);
-WiFiServer server_1(SERIAL1_TCP_PORT);
-WiFiServer *pServer[NUM_COM] = {&server_0, &server_1};
-WiFiClient TCPClient[NUM_COM][MAX_NMEA_CLIENTS];//1 tcp Server for multiple Client
+uint8_t buf[bufferSize];
+uint16_t nBuf = 0;//len of buf[]
 
+uint8_t arMsg[64];
+int msgsize = 0;
 
-uint8_t buf1[NUM_COM][bufferSize];
-uint16_t i1[NUM_COM] = {0, 0};
+// ********************* Helper functions **********************************
+void print_wakeup_reason() {
+	esp_sleep_wakeup_cause_t wakeup_reason;
 
-uint8_t buf2[NUM_COM][bufferSize];
-uint16_t i2[NUM_COM] = {0, 0};
+	wakeup_reason = esp_sleep_get_wakeup_cause();
 
-
-void setup() {
-
-  delay(500);
-
-  pCOM[0]->begin(UART_BAUD0, SERIAL_PARAM0, SERIAL0_RXPIN, SERIAL0_TXPIN);
-  pCOM[1]->begin(UART_BAUD1, SERIAL_PARAM1, SERIAL1_RXPIN, SERIAL1_TXPIN);
-
-  if (debug) 
-    Serial_1.println("\n\nPunch WiFi serial bridge V1.00");
-#ifdef MODE_AP
-  if (debug) 
-    Serial_1.println("Open ESP Access Point mode");
-  //AP mode (phone connects directly to ESP) (no router)
-  WiFi.mode(WIFI_AP);
-
-  WiFi.softAP(ssid, pw); // configure ssid and password for softAP
-  delay(2000); // VERY IMPORTANT
-  WiFi.softAPConfig(ip, ip, netmask); // configure ip address for softAP
-
-#endif
-
-
-#ifdef OTA_HANDLER
-  ArduinoOTA
-  .onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH)
-      type = "sketch";
-    else // U_SPIFFS
-      type = "filesystem";
-
-    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-    Serial_1.println("Start updating " + type);
-  })
-  .onEnd([]() {
-    Serial_1.println("\nEnd");
-  })
-  .onProgress([](unsigned int progress, unsigned int total) {
-    Serial_1.printf("Progress: %u%%\r", (progress / (total / 100)));
-  })
-  .onError([](ota_error_t error) {
-    Serial_1.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial_1.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial_1.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial_1.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial_1.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial_1.println("End Failed");
-  });
-  // if DNSServer is started with "*" for domain name, it will reply with
-  // provided IP to all DNS request
-
-  ArduinoOTA.begin();
-#endif // OTA_HANDLER    
-
-  Serial_1.println("Test COM0 or Serial_1.println: Data Transfer");
-  Serial_1.println("** This terminal act like a Punch MCU talking to Wifi module **");
-  Serial_1.println("Terminal (MCU) <---> WifiModule/Brigde <---> Wifi Client");
-  Serial_1.println("===== Waiting for Wifi/TCP connection/data <--> =====");
-  Serial_1.println("Punch UART0  <--> Wifi");
-  if (debug)
-    Serial_1.println("Starting TCP Server 0");
-  pServer[0]->begin(); // start TCP server
-  pServer[0]->setNoDelay(true);
-  Serial_1.println("UART1  Kit's USB port");
-  if (debug)
-    Serial_1.println("Starting TCP Server 1");
-  pServer[1]->begin(); // start TCP server
-  pServer[1]->setNoDelay(true);
-  //esp_err_t esp_wifi_set_max_tx_power(50);  //lower WiFi Power
+	switch (wakeup_reason)
+	{
+	case ESP_SLEEP_WAKEUP_EXT0: Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+	case ESP_SLEEP_WAKEUP_EXT1: Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+	case ESP_SLEEP_WAKEUP_TIMER: Serial.println("Wakeup caused by timer"); break;
+	case ESP_SLEEP_WAKEUP_TOUCHPAD: Serial.println("Wakeup caused by touchpad"); break;
+	case ESP_SLEEP_WAKEUP_ULP: Serial.println("Wakeup caused by ULP program"); break;
+	default: Serial.printf("Wakeup was not caused by deep sleep: %d\r\n", wakeup_reason); break;
+	}
+}
+	
+void print_wifi_error() {
+	switch (WiFi.status())
+	{
+	case WL_IDLE_STATUS: Serial.println("WL_IDLE_STATUS"); break;
+	case WL_NO_SSID_AVAIL: Serial.println("WL_NO_SSID_AVAIL"); break;
+	case WL_CONNECT_FAILED: Serial.println("WL_CONNECT_FAILED"); break;
+	case WL_DISCONNECTED: Serial.println("WL_DISCONNECTED"); break;
+	default: Serial.printf("No know WiFi error"); break;
+	}
 }
 
+//
+//********************* SETUP ****************************************
+//
+void setup() {
+	delay(500);
 
+	Serial.begin(UART_BAUD1, SERIAL_PARAM1, SERIAL1_RXPIN, SERIAL1_TXPIN);
+	serialMcu.begin(UART_BAUD0, SERIAL_PARAM0, SERIAL0_RXPIN, SERIAL0_TXPIN);
+
+	Serial.println();
+	//------- server connection  preferences ---------------
+	preferences.begin("Punch", false);
+	ssid = preferences.getString("ssid", DEF_SSID);
+	password = preferences.getString("password", DEF_PASSWORD);
+	ipServer = preferences.getLong("ipServer", DEF_IPSERVER);
+	portServer = preferences.getLong("portServer", DEF_PORTSERVER);
+	enableWifi  = preferences.getBool("enableWifi", DEF_ENABLEWIFI);
+	Serial.printf("Setup::Enable Wifi = %d\r\n", enableWifi);
+	preferences.end();
+	//------------------------------
+	serialMcu.flush();
+	//Explain ESP2 wakeup reason
+	print_wakeup_reason();
+	if(!enableWifi){
+		Serial.println("Wifi is disabled.");
+		Serial.println("...accepting messages from main mcu...");
+		return;
+	}
+
+	// Try connect WiFi network
+	WiFi.begin(ssid.c_str(), password.c_str());
+	Serial.println("");
+	Serial.printf("Connecting to wifi: ssid: %s  password: %s \r\n", ssid.c_str(), password.c_str());
+	int count_try = 0;
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+		count_try++;    // Increase try counter
+		if (count_try >= wifi_max_try) {
+			// Sorry but wifi is not available 
+			Serial.println("\n");
+			Serial.println("Impossible to establish WiFi connection.");
+
+			print_wifi_error();
+			Serial.println("Sleep a little and retry later, bye.");
+
+			// Set the wakeup 
+			esp_sleep_enable_timer_wakeup(duration_deep_sleep * 1000000);
+			// And ask processor to sleep now 
+			esp_deep_sleep_start();
+		}
+	}
+	Serial.println();
+	Serial.printf("Connected to network, my IP address: "); Serial.println(WiFi.localIP());
+
+	Serial.print("Connecting to server: ip="); Serial.print(ipServer);
+	Serial.printf(" port=%d\r\n", portServer);
+	if (!client.connect(ipServer, portServer, 12000)) {
+		Serial.println("Connection to server failed.");
+		Serial.println("...accepting messages from main mcu(USB)...");
+		delay(1000);
+		return;
+	}
+
+	Serial.println("Connect as client ok.");
+	Serial.println("======================================================");
+	Serial.println("Test COM1 or Serial.println: Data Transfer");
+	Serial.println("** This terminal act like a Punch MCU talking to Wifi module **");
+	Serial.println("Terminal (MCU) <---> WifiModule/Brigde <---> Wifi Client");
+	Serial.println("===== Waiting for Wifi/TCP connection/data <--> =====");
+	Serial.println("Punch UART  <--> Wifi");
+	//esp_err_t esp_wifi_set_max_tx_power(50);  //lower WiFi Power
+
+  //====================== OTA setup ======================================
+#ifdef OTA_HANDLER
+	ArduinoOTA
+		.onStart([]() {
+		String type;
+		if (ArduinoOTA.getCommand() == U_FLASH)
+			type = "sketch";
+		else // U_SPIFFS
+			type = "filesystem";
+
+		// NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+		Serial.println("Start updating " + type);
+			})
+		.onEnd([]() {
+				Serial.println("\nEnd");
+			})
+				.onProgress([](unsigned int progress, unsigned int total) {
+				Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+					})
+				.onError([](ota_error_t error) {
+						Serial.printf("Error[%u]: ", error);
+						if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+						else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+						else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+						else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+						else if (error == OTA_END_ERROR) Serial.println("End Failed");
+					});
+					// if DNSServer is started with "*" for domain name, it will reply with
+					// provided IP to all DNS request
+
+					ArduinoOTA.begin();
+#endif // OTA_HANDLER    
+}
+
+//=========================================================================
+
+bool ExecMessage(uint8_t* pMsg)
+{
+	if (pMsg[0] != 0xC0) return false;
+	Serial.printf("ExecMessage: data size %u\r\n",(uint8_t)pMsg[2]);
+	switch (pMsg[1]) {
+	//all setting require a reset/reboot **************
+	case ID_WIFI_SET_REMOTE_AP_SSID:
+		pMsg[3+30]=0;
+		ssid = (char*)&pMsg[3];	
+		//send response to mcu
+		pMsg[0]=RESPONSE;
+		pMsg[2]=0;
+		//serialMcu.write(pMsg, 3+pMsg[2]); // now send to UART for Wifi :
+		preferences.begin("Punch", false);
+		preferences.putString("ssid", ssid);
+		preferences.end();
+		//
+		Serial.printf("   SET_AP_SSID: %s\r\n",ssid.c_str());
+		break;
+	case ID_WIFI_SET_REMOTE_AP_PW :
+		pMsg[3+25]=0;
+		password = (char*)&pMsg[3];	
+		//send response to mcu
+		pMsg[0]=RESPONSE;
+		pMsg[2]=0;
+		//serialMcu.write(pMsg, 3+pMsg[2]); // now send to UART for Wifi :
+		preferences.begin("Punch", false);
+		preferences.putString("password", password.c_str());
+		preferences.end();
+		//
+		Serial.printf("   SET_AP_PW: %s\r\n",password.c_str());
+		break;
+	case ID_WIFI_SET_IP_PORT:
+		ipServer = *(uint32_t*)&pMsg[3];
+		portServer = *(uint16_t*)&pMsg[7];
+		//send response to mcu
+		pMsg[0]=RESPONSE;
+		pMsg[2]=0;
+		//serialMcu.write(pMsg, 3+pMsg[2]); // now send to UART for Wifi :
+		preferences.begin("Punch", false);
+		preferences.putLong("ipServer", ipServer);
+		preferences.putLong("portServer", portServer);
+		preferences.end();
+		//
+		Serial.printf("   SET_IP_PORT: port= %u  ip=",portServer);
+		Serial.println(ipServer);
+		Serial.println("Please reset the hardawre.");
+		break;
+	case ID_WIFI_ENABLE:{
+		//if(enableWifi == *(uint8_t*)&pMsg[3])
+			//break;
+		//else can use this msg to reset me.
+		enableWifi = *(uint8_t*)&pMsg[3];
+		preferences.begin("Punch", false);
+		preferences.putBool("enableWifi", enableWifi);
+		preferences.end();
+		//send response to mcu
+		pMsg[0]=RESPONSE;
+		pMsg[2]=0;
+		//serialMcu.write(pMsg, 3+pMsg[2]); // now send to UART for Wifi :
+		preferences.begin("Punch", false);
+		Serial.printf("Enable Wifi = %d\r\n", enableWifi);
+		preferences.end();
+		//
+		ESP.restart();
+		break;
+	}
+	case ID_WIFI_RESTART:
+		ESP.restart();
+		break;
+	default:
+		break;
+	}
+	return true;
+}
+
+int ixCur = 0;
+uint32_t ixTimer=0;
 void loop()
 {
 #ifdef OTA_HANDLER
-  ArduinoOTA.handle();
+	ArduinoOTA.handle();
 #endif // OTA_HANDLER
+	//===== UART <-- Wifie TCP ===========
+	// COM[0](MCU), COM[1] (debug output)
+	//=== Wifi(Punch App) ---> MCU UART0 ===
+	while (client.available())
+	{
+		buf[nBuf] = client.read(); // read char from TCP Wifi client
+		if (nBuf < bufferSize - 1)
+			nBuf++;
+	}
+	//---------------------------------------------------------
+	serialMcu.write(buf, nBuf); // now send to UART for Wifi :
+	nBuf = 0;
+	//
+	//=== MCU UART0 ---> Wifi(Punch App)==========================
+	//
+	while (serialMcu.available())
+	{
+		buf[nBuf] = serialMcu.read(); // read char from UART
+		if (nBuf < bufferSize - 1) nBuf++;
+	}
+	//---- intercept and check for Punch message ---------
+	if ((msgsize + nBuf) > 64){//something is wrong!
+		Serial.println("msgsize>64");
+		memset(arMsg,0,sizeof(arMsg));
+		msgsize = 0;
+	}
+	if (nBuf > 0) {
+		memcpy(&arMsg[msgsize], buf, nBuf);
+		msgsize += nBuf;
+	}
+	if (msgsize > 0 && arMsg[0] == 0xC0) {
+		Serial.printf("1:got cmd 0xC0 msgsize=%d\r\n",msgsize);
+		if (msgsize > 1){
+			if((arMsg[1] == ID_WIFI_SET_REMOTE_AP_SSID || arMsg[1] == ID_WIFI_SET_REMOTE_AP_PW ||
+							arMsg[1] == ID_WIFI_SET_IP_PORT || arMsg[1] == ID_WIFI_ENABLE)) {
+				if (msgsize > 2 && msgsize >= arMsg[2] + 3) {
+					Serial.printf("3:Got Wifi msgsize=%d MsgId=0x%x data=%u count=%d\r\n",msgsize,arMsg[1],arMsg[2],ixCur++);
+					ExecMessage(arMsg);
+					memset(arMsg,0,sizeof(arMsg));
+					msgsize = 0;
+					serialMcu.flush();
+					return;//do not pass on to wifi
+				}
+				//get next byte until arMsg[2]==datasize
+				Serial.printf("2:got msgid msgsize=%d...wait for data\r\n",msgsize);
+				return;//do not pass on to wifi
+			}
+			else {
+				msgsize=0;
+				memset(arMsg,0,sizeof(arMsg));
+				Serial.println("4: bad or unknown msg");
+				serialMcu.flush();
+				return;
+			}
+		}
+		//else	msgsize=0;
+		Serial.println("5:wait for data");
+		delay(10);
+		if(ixTimer++ > 10){//reset msg
+			memset(arMsg,0,sizeof(arMsg));
+			msgsize = 0;
+			ixTimer = 0;
+			serialMcu.flush();
+			Serial.println("recv msg timeout - reset");
+		}
+		return;//do not pass on to wifi
+	}
+	else msgsize = 0;
+	//----------------------
+	client.write(buf, nBuf); // now send to TCP WiFi client
+	nBuf = 0;
 
-  //======= check TCP client connection =====
-  for (int num = 0; num < NUM_COM ; num++)
-  {
-    byte i=0;
-    if (pServer[num]->hasClient())
-    {
-      for (i = 0; i < MAX_NMEA_CLIENTS; i++) {
-        //find free/disconnected spot
-        if (!TCPClient[num][i] || !TCPClient[num][i].connected()) {
-          if (TCPClient[num][i])
-            TCPClient[num][i].stop();
-          TCPClient[num][i] = pServer[num]->available();
-          if (debug) {
-            Serial_1.print("New client for Server ");
-            Serial_1.print(num);
-            Serial_1.print("  Slot ");
-            Serial_1.println(i);
-          }
-          break;
-        }
-        else{
-          if (debug){
-            Serial_1.print("Client slot accupied Server ");
-            Serial_1.print(num);
-            Serial_1.print("  Slot ");
-            Serial_1.println(i);
-          }
-        }
-      }
-      if(i>=MAX_NMEA_CLIENTS)
-        Serial_1.print("No slot available for new Client - rejected ");
-      //no free/disconnected spot so reject
-      WiFiClient TmpserverClient = pServer[num]->available();
-      TmpserverClient.stop();
-    }
-  }
-  //===== UART <--> Wifie TCP ===========
-  // COM[0](MCU), COM[1] (debug output)
-  for (int num = 0; num < NUM_COM ; num++)
-  {
-    if (pCOM[num] != NULL)
-    {
-      //=== Wifi(Punch App) ---> MCU UART0 ===
-      for (byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
-      {
-        if (TCPClient[num][cln])
-        {
-          while (TCPClient[num][cln].available())
-          {
-            buf1[num][i1[num]] = TCPClient[num][cln].read(); // read char from TCP Wifi client
-            if (i1[num] < bufferSize - 1) 
-              i1[num]++;
-          }
-          pCOM[num]->write(buf1[num], i1[num]); // now send to UART(num) :
-          i1[num] = 0;
-        }
-      }
-      //=== MCU UART0 ---> Wifi(Punch App)===
-      if (pCOM[num]->available())
-      {
-        while (pCOM[num]->available())
-        {
-          buf2[num][i2[num]] = pCOM[num]->read(); // read char from UART
-          if (i2[num] < bufferSize - 1) i2[num]++;
-        }
-        for (byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
-        {
-          if (TCPClient[num][cln]) 
-            TCPClient[num][cln].write(buf2[num], i2[num]); // now send to TCP WiFi client
-        }
-        i2[num] = 0;
-      }
-    }
-  }
 }
--- src/config.h
+++ src/config.h
@@ -2,43 +2,51 @@
 // 
 //#define BLUETOOTH
 #define OTA_HANDLER 
-#define MODE_AP // phone connects directly to ESP
+//#define MODE_AP // phone connects directly to ESP
 #define PROTOCOL_TCP
 
 bool debug = true;
 
 #define VERSION "1.00"
 
-// For AP mode:
-const char *ssid = "Punch";  // You will connect your TCP client to this Access Point
-const char *pw = "YouAndMe"; // and this is the password
-IPAddress ip(192, 168, 5, 1); // toto - change it if needed
+#define DEF_SSID    "eMedia Net_EXT" //host wifi AP
+#define DEF_PASSWORD "thanhtruong"//host wifi AP
+#define DEF_IPSERVER IPAddress(192, 168, 1, 15) //host server
+#define DEF_PORTSERVER 9500
+#define DEF_ENABLEWIFI true
 IPAddress netmask(255, 255, 255, 0);
+String ssid = DEF_SSID; //host wifi
+String password = DEF_PASSWORD;//host wifi
+IPAddress ipServer = DEF_IPSERVER;//host wifi
+uint16_t portServer =  DEF_PORTSERVER ;
+bool enableWifi = DEF_ENABLEWIFI;
 
-// You must connect the phone to this AP, then:
-// menu -> connect -> Internet(TCP) -> 192.168.5.1:8880  for UART0 (main Punch com, as if it was USB connection)
-//                                  -> 192.168.5.1:8881  for UART1 (for debugging or other use, can be use similiar to UART0)
-
-
-
-
-#define MAX_NMEA_CLIENTS 3          //per server(uart)
-#define NUM_COM   2                 // total number of COM Ports
+#define COMMAND 0xC0
+#define RESPONSE 0xD0
+// message from mcu ----------------------
+#define	ID_WIFI_SET_REMOTE_AP_SSID 	0x21 //set 30 bytes ssid, including NULL
+#define	ID_WIFI_GET_REMOTE_AP_SSID 	0x22//return 30 bytes ssid, including NULL
+#define	ID_WIFI_SET_REMOTE_AP_PW 	0x23	//set 20 bytes password including NULL
+#define	ID_WIFI_GET_REMOTE_AP_PW 	0x24	//return 20 bytes password including NULL
+#define	ID_WIFI_SET_IP_PORT  		0x25 	//remote access point (PC-tcp socker server): ip and port
+										//set 4 bytes ipAddr, 2 byte port#
+#define ID_WIFI_GET_IP_PORT  		0x26		//return 4 bytes ipAddr, 2 byte port#
+// 
+#define ID_WIFI_ENABLE				0x27 //disable wifi, just use usart to communicate with main mcu for messaging.
+#define ID_WIFI_RESTART				0x28 //reset me - not use yet
 /*************************  COM Port 0 *******************************/
 // Doit Kit:COM2<-->CP210x (jumper (pc COM5)):tobe connect to MCU ---
 #define UART_BAUD0 115200           // Baudrate UART2
 #define SERIAL_PARAM0 SERIAL_8N1    // Data/Parity/Stop UART2
 #define SERIAL0_RXPIN 16            // receive Pin UART2
 #define SERIAL0_TXPIN 17            // transmit Pin UART2
-#define SERIAL0_TCP_PORT 8880       // Wifi Port UART2
 /*************************  COM Port 1 *******************************/
 // Doit Kit:USB<-->CP210x (on board mini USB (pc COM8)) -----------
 #define UART_BAUD1 115200           // Baudrate UART0
 #define SERIAL_PARAM1 SERIAL_8N1    // Data/Parity/Stop UART0
 #define SERIAL1_RXPIN 3             // receive Pin UART0
 #define SERIAL1_TXPIN 1             // transmit Pin UART0
-#define SERIAL1_TCP_PORT 8881       // Wifi Port UART0
 //--------------------------
-#define bufferSize 1024
+#define bufferSize 64
 
 //////////////////////////////////////////////////////////////////////////
